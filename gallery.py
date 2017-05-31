9780995aff4c59bac3803102b1735c5a6fd8d134
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2017-05-31 17:32:09 -0300

    Add create thumbs

--- gallery.py
+++ gallery.py
@@ -2,17 +2,35 @@ import argparse
 import os
 import tinify
 from decouple import config
+from PIL import Image
+from resizeimage import resizeimage
 
 class Main():
     def __init__(self, args):
         tinify.key = config('TINYPNG_KEY')
         self.images_srcpath = args.src
         self.output_path = args.output
+        self.output_gallery = os.path.join(self.output_path, 'galeria')
+        self.output_thumbs = os.path.join(self.output_path, 'galeria-thumbs')
 
     def main(self):
         self._create_output_dir()
-        src_output_filepaths = self._get_filespaths()
-        self._resize_and_compress(src_output_filepaths)
+        self._resize_and_compress(self._get_filespaths())
+        self._create_thumbs(self._get_filespaths())
+
+    def _create_thumbs(self, output_filespaths):
+        print('')
+        print('Creating thumbnails')
+        for _,filepath in output_filespaths:
+            output_thumbname = os.path.basename(filepath)
+            output_thumbpath = os.path.join(self.output_thumbs,
+                                            output_thumbname)
+
+            print(output_thumbpath)
+            with open(filepath, 'r+b') as f:
+                with Image.open(f) as image:
+                    cover = resizeimage.resize_thumbnail(image, [100, 100])
+                    cover.save(output_thumbpath, image.format)
 
     def _resize_and_compress(self, src_output_filepaths):
         for src_filepath, output_filepath in src_output_filepaths:
@@ -24,6 +42,10 @@ class Main():
     def _create_output_dir(self):
         if not os.path.exists(self.output_path):
             os.mkdir(self.output_path)
+        if not os.path.exists(self.output_gallery):
+            os.mkdir(self.output_gallery)
+        if not os.path.exists(self.output_thumbs):
+            os.mkdir(self.output_thumbs)
 
     def _get_srcfilepaths(self, src_files):
         return [file.path for file in src_files]
@@ -31,7 +53,8 @@ class Main():
     def _get_output_filepaths(self, src_files):
         output_names = ['{:03d}.{}'.format(index+1, file.name[-3:])
                         for index, file in enumerate(src_files)]
-        return [os.path.join(self.output_path, output_name)
+        print(output_names)
+        return [os.path.join(self.output_gallery, output_name)
                 for output_name in output_names]
 
     def _get_filespaths(self):
